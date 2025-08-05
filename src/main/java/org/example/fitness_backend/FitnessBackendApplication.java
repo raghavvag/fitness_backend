2a2c2b2bae51b4ec658c72fc0bc692759bf55643
Raghav Agrawal <raghav.github69@gmail.com>
2025-08-05 11:59:21 +0530

    workspace built

--- src/main/java/org/example/fitness_backend/FitnessBackendApplication.java
+++ src/main/java/org/example/fitness_backend/FitnessBackendApplication.java
@@ -8,6 +8,7 @@ public class FitnessBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FitnessBackendApplication.class, args);
+        System.out.println("Hello World!");
     }
 
 }
